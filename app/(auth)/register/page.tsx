9d96db8408c2fd82c5958009f7c888a02ee9aa16
Shravan Chaudhary <shravansinghchaudhary@gmail.com>
2023-03-26 14:05:11 +0530

    feat: :tada: Initial Commit

---
+++ app/(auth)/register/page.tsx
@@ -0,0 +1,3 @@
+export default function Register() {
+  return <div className='text-4xl'>Register Page</div>
+}
---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,19 @@
+import '@/styles/global.css'
+import GlassPane from '@/components/GlassPane'
+
+export const metadata = {
+  title: 'Dashboard | Project Manager',
+  description: 'A great project management app',
+}
+
+export default function DashboardRootLayout({ children }) {
+  return (
+    <html lang='en'>
+      <body className='h-screen w-screen rainbow-mesh p-6 '>
+        <GlassPane className='w-full h-full flex items-center justify-center'>
+          {children}
+        </GlassPane>
+      </body>
+    </html>
+  )
+}
